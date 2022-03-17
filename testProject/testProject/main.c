57ff5548e4475fa4e7d110302aba4d5c641dec90
C1Ron <ronny@fecreate.com>
2022-03-17 11:55:12 +0100

    fix bad merge due to amend

--- testProject/testProject/main.c
+++ testProject/testProject/main.c
@@ -3,13 +3,9 @@
 int main()
 {
 	printf("Hello world\n");
-<<<<<<< HEAD
+
 	int16_t addedValue = myFunction(-20000, -13000);
 	printf("addedValue = %i", addedValue);
 
-=======
-	int16_t addedValue = myFunction(20000, 10000);
-	printf("addedValue = %i", addedValue);
->>>>>>> 039091bcd96a17310d2901610dab381105cfcfb1
 	return 0;
 }
--- testProject/testProject/main.h
+++ testProject/testProject/main.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 int16_t myFunction(int16_t in1, int16_t in2)
 {
-<<<<<<< HEAD
+
 	if (in1 + in2 > 32000) {
 		return 32000;
 	}
@@ -12,7 +12,5 @@ int16_t myFunction(int16_t in1, int16_t in2)
 	else {
 		return in1 + in2;
 	}
-=======
-	return (int16_t)(in1 + in2);
->>>>>>> 039091bcd96a17310d2901610dab381105cfcfb1
+
 }
