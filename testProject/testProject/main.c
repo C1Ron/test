039091bcd96a17310d2901610dab381105cfcfb1
C1Ron <ronny@fecreate.com>
2022-03-17 11:24:41 +0100

    added myFunction

--- testProject/testProject/main.c
+++ testProject/testProject/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include "main.h"
 int main()
 {
-	printf("Hello world");
+	printf("Hello world\n");
+	int16_t addedValue = myFunction(20000, 10000);
+	printf("addedValue = %i", addedValue);
 	return 0;
 }
---
+++ testProject/testProject/main.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdint.h>
+int16_t myFunction(int16_t in1, int16_t in2)
+{
+	return (int16_t)(in1 + in2);
+}
